d1cb87a00f938de564e5a1bcd9a167e79148c370
rchbinou <SPh9.BOr453E>
2021-11-02 14:36:47 +0100

    add capacitor (mobile app build)

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'radhouen.recipe.app',
+  appName: 'recipe-app',
+  webDir: 'dist/recipe-app',
+  bundledWebRuntime: false
+};
+
+export default config;
